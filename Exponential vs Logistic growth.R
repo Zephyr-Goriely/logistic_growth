e128415f5e6a60edc406acc21d5ac3def605fa29
Hidden <150150268+hiddenuser3@users.noreply.github.com>
2023-12-06 17:15:35 +0000

    Update Exponential vs Logistic growth.R

--- Exponential vs Logistic growth.R
+++ Exponential vs Logistic growth.R
@@ -19,7 +19,7 @@ exponential_fun <- function(t) {
 }
 
 # Defining the model estimates using the values from the previous exercise
-N0 <- 6.903e+00 #initial population size
+N0 <- 995.256 #initial population size
 
 r <- 9.990e-03 #gradient
 
@@ -29,11 +29,12 @@ K <- 5.903e+10 #carrying capacity
 ggplot(data = growth_data, aes(t,N)) +
   geom_function(fun=logistic_fun, linetype = "solid", aes(color = "Logistic")) +
   geom_function(fun=exponential_fun, linetype = "dashed", aes(color = "Exponential")) +
-  xlim(1500,3200) +
+  xlim(1250,2500) +
   ylim(0, 6e+10) +
   labs(title = "Exponential vs Logistic growth",
        x = "Time",
        y = "Population Size",
        color = "Function Type") +
   scale_color_manual(values = c("red", "blue"), name = "Function Type")
+
 
