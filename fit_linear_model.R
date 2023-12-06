ad50a5332e4cf6fbfb67baa8f6d9dd26becfd396
Hidden <150150268+hiddenuser3@users.noreply.github.com>
2023-12-06 17:17:35 +0000

    Update fit_linear_model.R

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -12,6 +12,7 @@ data_subset1 <- growth_data %>% filter(t<1600) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
+(intercept = N_log)
 
 # Intercept plotted as 6.903e+00 and gradient estimated at 9.990e-03
 
